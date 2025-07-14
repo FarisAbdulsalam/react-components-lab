9378e711a70d73c7a6cc8cae113157f950ecb01e
Faris <farisalmannaei@outlook.com>
2025-07-14 07:40:59 +0300

    done

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import './WeatherForecast.css';
+import WeatherIcon from './WeatherIcon.jsx';
+import WeatherData from './WeatherData.jsx';
+
+const WeatherForecast = ({forecast}) => {
+    return (
+        <>
+            <div className='weather'>
+                <WeatherIcon forecast={forecast}/>
+                <WeatherData forecast={forecast}/>
+            </div>
+        </>
+    )
+};
+
+export default WeatherForecast;
